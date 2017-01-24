2b4b5c3b7ef39473df55b24dd3a92d64b6794310
IPJeong <jip890611@naver.com>
2017-01-24 15:37:10 +0900

    파일업로드 관련 소스추가

--- CoolEngineer/src/main/java/com/engineers/moyeo/main/common/Code.java
+++ CoolEngineer/src/main/java/com/engineers/moyeo/main/common/Code.java
@@ -2,6 +2,26 @@ package com.engineers.moyeo.main.common;
 
 public interface Code {
 
-	public final static String imgPath = "C:\\moyeo_image";
+	// 사진저장 메인경로
+	public final static String rootPath = "c:\\dev\\moyeo_image\\";
+	
+	// 모임후기 사진저장경로
+	public final static String postImgPath = rootPath + "post\\img\\";
+	// 모임후기 동영상저장경로
+	public final static String postVideoPath = rootPath + "post\\video\\";
+
+	// 이벤트 메인사진 저장경로
+	public final static String eventImgPath = rootPath + "event\\mainImg\\";
+	// 이벤트 사진 저장경로
+	public final static String eventImgsPath = rootPath + "event\\img\\";
+	
+	// 모임신고사진 저장경로
+	public final static String reportImgPaht = rootPath + "reportImg\\";
+	
+	// 회원의 프로필사진 저장경로
+	public final static String profileImgPath = rootPath + "profileImg\\"; 
+	
+	// 장소사진의 저장경로
+	public final static String placeImgPath = rootPath + "placeImg\\";
 	
 }
---
+++ CoolEngineer/src/main/java/com/engineers/moyeo/main/model/FileForm.java
@@ -0,0 +1,28 @@
+package com.engineers.moyeo.main.model;
+
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class FileForm {
+	
+	private List<MultipartFile> files;
+	private String upDir;
+
+	public List<MultipartFile> getFiles() {
+		return files;
+	}
+
+	public String getUpDir() {
+		return upDir;
+	}
+
+	public void setUpDir(String upDir) {
+		this.upDir = upDir;
+	}
+
+	public void setFiles(List<MultipartFile> files) {
+		this.files = files;
+	}
+
+}
