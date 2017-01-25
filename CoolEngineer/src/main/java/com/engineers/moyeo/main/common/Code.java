594da931771dcc0ec5e83a1ed12324e326446b32
IPJeong <jip890611@naver.com>
2017-01-25 17:44:14 +0900

    코드수정

--- CoolEngineer/src/main/java/com/engineers/moyeo/main/common/Code.java
+++ CoolEngineer/src/main/java/com/engineers/moyeo/main/common/Code.java
@@ -4,24 +4,24 @@ public interface Code {
 
 	// 사진저장 메인경로
 	public final static String rootPath = "c:\\dev\\moyeo_image\\";
-	
+	  
 	// 모임후기 사진저장경로
-	public final static String postImgPath = rootPath + "post\\img\\";
+	public final static String postImgPath = "post\\img\\";
 	// 모임후기 동영상저장경로
-	public final static String postVideoPath = rootPath + "post\\video\\";
+	public final static String postVideoPath = "post\\video\\";
 
 	// 이벤트 메인사진 저장경로
-	public final static String eventImgPath = rootPath + "event\\mainImg\\";
+	public final static String eventImgPath = "event\\mainImg\\";
 	// 이벤트 사진 저장경로
-	public final static String eventImgsPath = rootPath + "event\\img\\";
+	public final static String eventImgsPath = "event\\img\\";
 	
 	// 모임신고사진 저장경로
-	public final static String reportImgPaht = rootPath + "reportImg\\";
+	public final static String reportImgPaht = "reportImg\\";
 	
 	// 회원의 프로필사진 저장경로
-	public final static String profileImgPath = rootPath + "profileImg\\"; 
+	public final static String profileImgPath = "profileImg\\"; 
 	
 	// 장소사진의 저장경로
-	public final static String placeImgPath = rootPath + "placeImg\\";
+	public final static String placeImgPath = "placeImg\\";
 	
 }
